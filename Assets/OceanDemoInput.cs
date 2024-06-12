a79b3d4dbf7952f3a5ff3396fe2dd78649b8b36b
杨皓文 <steven@macbook.m3>
2024-06-12 20:50:48 +0800

    优化到任何环节都34帧以上

--- Assets/OceanDemoInput.cs
+++ Assets/OceanDemoInput.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class OceanDemoInput : MonoBehaviour
 {
     
     public bool PressYToReloadScene = true;
+
+    public Transform Portal;
+    public Transform Head;
+    
+    public PostProcessLayer PostProcessLayer;
+    public bool EnablePostProcessing
+    {
+        set => PostProcessLayer.enabled = value;
+        get => PostProcessLayer.enabled;
+    }
 
     // Update is called once per frame
     void Update()
@@ -15,5 +26,11 @@ public class OceanDemoInput : MonoBehaviour
         {
             UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
         }
+        
+        Vector3 relativePosition = Head.transform.position - Portal.transform.position;
+        Vector3 PortalForward = Portal.transform.forward;
+        float relativeDistance = Vector3.Dot(relativePosition, PortalForward);
+        
+        //Debug.Log(relativeDistance);
     }
 }
